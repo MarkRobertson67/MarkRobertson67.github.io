320852677a0e1bbe3a2d7fd01abaa7872c65b6d5
Mark Robertson <markrobertson67@gmail.com>
2025-04-01 23:28:59 -0400

    Adjusting Home background image sizing for desktop and mobile

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import MarkRobertsonPortfolio from "../../images/HomeImage.png";
-import "./Home.css"; 
+import "./Home.css";
 
 function Home() {
   return (
     <Link to="/projects" className="block">
       <div
-        className="relative flex flex-col justify-between min-h-[calc(100vh-5rem)] overflow-hidden text-white"
-        style={{
-          backgroundImage: `url(${MarkRobertsonPortfolio})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
+        className="relative flex flex-col justify-between min-h-[calc(100vh-5rem)] overflow-hidden text-white 
+                   bg-center bg-no-repeat bg-cover md:bg-[length:100%_100%]"
+        style={{ backgroundImage: `url(${MarkRobertsonPortfolio})` }}
       >
         {/* Top section with your main heading */}
-        <div className="pt-[17rem] md:pt-[17rem] text-center">
-        <h1 className="text-4xl md:text-7xl font-bold mb-4 animate-bounce">
+        <div className="pt-[16.5rem] md:pt-[21rem] text-center">
+          <h1 className="text-4xl md:text-7xl font-bold mb-4 animate-bounce">
             <span className="neon-text">PORTFOLIO</span>
           </h1>
         </div>
