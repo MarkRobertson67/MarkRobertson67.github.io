f930a7c4e7dc636d2b786a5e3ff2547d698557b4
Mark Robertson <markrobertson67@gmail.com>
2025-04-23 21:58:36 -0400

    Testing image in mobile.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import MarkRobertsonPortfolio from "../../images/MarkRobertsonPortfolio.png";
-import "./Home.css";
+// import "./Home.css";
 
 
 function Home() {
   return (
     <Link to="/projects" className="block">
-      <div w-full h-full object-cover style={{ height: 'calc(100vh - 70px)' }}>
+      <div style={{ height: 'calc(100vh - 70px)' }}>
         <img 
           src={MarkRobertsonPortfolio} 
           alt="Mark Robertson Portfolio"
-          className="img w-full h-full object-cover"
+          className="h-full w-full"
         />
       </div>
     </Link>
