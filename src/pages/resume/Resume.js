e179d3be03a4af4e4b10d84ebabb93a91873bbeb
Mark Robertson <markrobertson67@gmail.com>
2025-12-20 15:43:44 -0500

    Updated Resume

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -2,11 +2,10 @@ import React from "react";
 
 
 function Resume() {
-  const resumeUrl = "/Data_Analyst_Mark_Robertson_Resume.pdf";
+  const resumeUrl = "/Data_Analyst_Mark Robertson_Resume.pdf";
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 pb-16 bg-gray-100 dark:bg-gray-600">
-
 
       {/* PDF Viewer with Fallback */}
       <div className="w-full max-w-4xl border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-600">
