e3198e697a4eed84b0bf12e4cda50e7966c153aa
Mark Robertson <markrobertson67@gmail.com>
2025-12-20 15:50:13 -0500

    Updated Home Photo

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import MarkRobertsonPortfolio from "../../images/MarkRobertsonPortfolio.png";
+//import MarkRobertsonPortfolio from "../../images/MarkRobertsonPortfolio.png";
+import MarkRobertsonPortfolio from "../../images/DataAnalysis.png";
 import "./Home.css";
 
 function Home() {
